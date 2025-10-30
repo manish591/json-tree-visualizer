8d0680733b51b9a5e9eef8a2621baac916f0ee13
Manish Devrani <manishdevrani777@gmail.com>
2025-10-30 19:48:45 +0530

    added debounced node search

--- src/components/general/node-search.tsx
+++ src/components/general/node-search.tsx
@@ -1,41 +1,43 @@
-import { type BuiltInEdge, type Node, useReactFlow } from "@xyflow/react";
-import { useEffect, useState } from "react";
-import { toast } from "sonner";
-import { Input } from "@/components/ui/input";
+import { type BuiltInEdge, type Node, useReactFlow } from '@xyflow/react';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
+import { Input } from '@/components/ui/input';
+import { useDebounce } from '@/hooks/useDebounce';
 
 export function NodeSearch() {
-	const [query, setQuery] = useState("");
-	const { getNodes, fitView, setNodes } = useReactFlow<Node, BuiltInEdge>();
+  const [query, setQuery] = useState('');
+  const debouncedQuery = useDebounce(query);
+  const { getNodes, fitView, setNodes } = useReactFlow<Node, BuiltInEdge>();
 
-	useEffect(() => {
-		if (query) {
-			const node = getNodes().find((node) => node.id === query);
+  useEffect(() => {
+    if (debouncedQuery) {
+      const node = getNodes().find((node) => node.id === debouncedQuery);
 
-			if (node) {
-				setNodes((nodes) =>
-					nodes.map((n) =>
-						n.id === query
-							? { ...n, selected: true }
-							: { ...n, selected: false },
-					),
-				);
-				fitView({ nodes: [node], duration: 500 });
-			} else {
-				toast.error("No matching nodes found");
-			}
-		} else {
-			setNodes((nodes) => nodes.map((n) => ({ ...n, selected: false })));
-		}
-	}, [query, fitView, setNodes, getNodes]);
+      if (node) {
+        setNodes((nodes) =>
+          nodes.map((n) =>
+            n.id === debouncedQuery
+              ? { ...n, selected: true }
+              : { ...n, selected: false },
+          ),
+        );
+        fitView({ nodes: [node], duration: 500 });
+      } else {
+        toast.error('No matching nodes found');
+      }
+    } else {
+      setNodes((nodes) => nodes.map((n) => ({ ...n, selected: false })));
+    }
+  }, [debouncedQuery, fitView, setNodes, getNodes]);
 
-	return (
-		<Input
-			placeholder="Search node"
-			className="h-8"
-			value={query}
-			onChange={(e) => {
-				setQuery(e.target.value);
-			}}
-		/>
-	);
+  return (
+    <Input
+      placeholder="Search node"
+      className="h-8"
+      value={query}
+      onChange={(e) => {
+        setQuery(e.target.value);
+      }}
+    />
+  );
 }
